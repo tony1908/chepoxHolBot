9a99d1f81d7989107e96df81c4da073c6992340d
Antonio <toony1908@gmail.com>
2016-05-08 03:28:53 -0500

    respuesta del bot

--- index.js
+++ index.js
@@ -5,18 +5,38 @@ var jsonParser = bodyParser.json();
 var packageInfo = require('./package.json');
 
 var app = express();
+var token = "EAAOCWDJppPcBAPECyKIvDxQ4JtWH9ZC7EicbJZAXoZBa3jIJV3F4uO4jT38IoBz3GrZB8x5paSw2dzdp0tS2juF75XZC2J3jpYv8LOoJZB0BayQgoWnVeZBPwSsWqAdwCg4mSYotoI84qpnYeKPMX4TLolylJynigDZCBKSQbxO8swZDZD"
 
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'chepo_bot') {
-    res.send(req.query['hub.challenge']);
+// app.get('/webhook/', function (req, res) {
+//   if (req.query['hub.verify_token'] === 'chepo_bot') {
+//     res.send(req.query['hub.challenge']);
+//   }
+//   res.send('Error, wrong validation token');
+// })
+
+app.post('/webhook/', jsonParser,  function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    console.log(event)
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      	console.log(event.message.text)
+        sendTextMessage(sender, "Welcome to this new bot");
+        continue;
+      }
+      // Handle a text message from this sender
+    }
   }
-  res.send('Error, wrong validation token');
-})
+  res.sendStatus(200);
+});
+
 
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
