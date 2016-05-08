dd2c78a5917c2d4825da7ba7994858f98e87f80e
Antonio <toony1908@gmail.com>
2016-05-08 03:35:30 -0500

    respuesta del bot

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ app.get('/', function (req, res) {
 //   res.send('Error, wrong validation token');
 // })
 
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.post('/webhook/', jsonParser,  function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
